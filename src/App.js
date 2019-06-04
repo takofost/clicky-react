8addddc3c9bf0bc338e5121cfa3cf728188f886e
Kenny Tran <trank7295@gmail.com>
2019-06-04 18:18:34 -0400

    added files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    friends
+  };
+
+  removeFriend = id => {
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    this.setState({ friends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,30 @@
+import android1 from './images/android1.png';
+import android2 from './images/android2.png';
+import android3 from './images/android3.png';
+import android4 from './images/android4.png';
+import android5 from './images/android5.png';
+import android6 from './images/android6.png';
+import android7 from './images/android7.png';
+import android8 from './images/android8.png';
+import android9 from './images/android9.png';
+import android10 from './images/android10.png';
+import android11 from './images/android11.png';
+import android12 from './images/android12.png';
+
+const images = [
+  android1,
+  android2,
+  android3,
+  android4,
+  android5,
+  android6,
+  android7,
+  android8,
+  android9,
+  android10,
+  android11,
+  android12
+
+];
+
+export default images;
